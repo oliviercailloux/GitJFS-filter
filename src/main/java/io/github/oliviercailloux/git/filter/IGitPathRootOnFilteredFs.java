3e9aca0c7fe46ba2e8afda10ddf1c24a31680a5e
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-12-23 10:01:56 +0100

    Init (from st-project)

---
+++ src/main/java/io/github/oliviercailloux/git/filter/IGitPathRootOnFilteredFs.java
@@ -0,0 +1,9 @@
+package io.github.oliviercailloux.git.filter;
+
+import io.github.oliviercailloux.gitjfs.GitPathRoot;
+
+sealed interface IGitPathRootOnFilteredFs extends
+    IGitPathOnFilteredFs permits GitPathRootOnFilteredFs, GitPathRootRefOnFilteredFs, GitPathRootShaOnFilteredFs, GitPathRootShaCachedOnFilteredFs {
+  @Override
+  GitPathRoot delegate();
+}
