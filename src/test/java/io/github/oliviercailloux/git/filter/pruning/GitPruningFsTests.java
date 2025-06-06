da1f5d21abbdc86b9d50938740d7b801c8285b1b
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-06-06 23:14:41 +0200

    mv around

--- src/test/java/io/github/oliviercailloux/git/filter/pruning/GitPruningFsTests.java
+++ src/test/java/io/github/oliviercailloux/git/filter/pruning/GitPruningFsTests.java
@@ -1,226 +1,153 @@
 package io.github.oliviercailloux.git.filter.pruning;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.graph.Graph;
-import com.google.common.graph.GraphBuilder;
-import com.google.common.graph.MutableGraph;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.UnmodifiableIterator;
+import com.google.common.graph.ImmutableGraph;
+import io.github.oliviercailloux.git.factory.JGit;
+import io.github.oliviercailloux.git.filter.pruning.GitPruningFs;
+import io.github.oliviercailloux.gitjfs.GitDfsFileSystem;
+import io.github.oliviercailloux.gitjfs.GitFileSystemProvider;
+import io.github.oliviercailloux.gitjfs.GitPathRootSha;
 import io.github.oliviercailloux.gitjfs.GitPathRootShaCached;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.spi.FileSystemProvider;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepository;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
 import org.eclipse.jgit.lib.ObjectId;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GitPruningFsTests {
+  @SuppressWarnings("unused")
+  private static final Logger LOGGER = LoggerFactory.getLogger(GitPruningFsTests.class);
 
   @Test
-  void testPruneGraphWithEmptyInputs() {
-    Graph<GitPathRootShaCached> emptyGraph = GraphBuilder.directed().build();
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of(ObjectId.fromString("a1b2c3d4e5f6789012345678901234567890abcd"));
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(emptyGraph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    assertTrue(result.nodes().isEmpty());
-    assertEquals(0, result.edges().size());
-  }
+  void testRead() throws Exception {
+    try (DfsRepository repo = new InMemoryRepository(new DfsRepositoryDescription("myrepo"))) {
+      final ImmutableList<ObjectId> commits = JGit.createRepoWithSubDir(repo);
+      assertEquals(3, commits.size());
+      try (GitDfsFileSystem fs =
+          GitFileSystemProvider.instance().newFileSystemFromDfsRepository(repo)) {
+        LOGGER.debug("Shas: " + fs.graph().nodes());
 
-  private static GitPathRootShaCached mockNode(String sha) {
-    String fullSha = sha.repeat(40).substring(0, 40);
-    GitPathRootShaCached node = Mockito.mock(GitPathRootShaCached.class);
-    Mockito.when(node.getStaticCommitId()).thenReturn(ObjectId.fromString(fullSha));
-    return node;
-  }
-
-  @Test
-  void testPruneGraphIdentical() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root1 = mockNode("1");
-    GitPathRootShaCached diamond1 = mockNode("2");
-    GitPathRootShaCached diamond2 = mockNode("3");
-    GitPathRootShaCached diamond3 = mockNode("4");
-    
-    GitPathRootShaCached root2 = mockNode("5");
-    
-    GitPathRootShaCached root3 = mockNode("6");
-    GitPathRootShaCached line1 = mockNode("7");
-    
-    graph.addNode(root1);
-    graph.addNode(diamond1);
-    graph.addNode(diamond2);
-    graph.addNode(diamond3);
-    graph.putEdge(root1, diamond1);
-    graph.putEdge(root1, diamond2);
-    graph.putEdge(diamond1, diamond3);
-    graph.putEdge(diamond2, diamond3);
-
-    graph.addNode(root2);
-    
-    graph.addNode(root3);
-    graph.addNode(line1);
-    graph.putEdge(root3, line1);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of();
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    assertEquals(7, result.nodes().size());
-    assertEquals(5, result.edges().size());
-    assertEquals(graph, result);
+        final GitPruningFs all = GitPruningFs.prune(fs, c -> false);
+        final GitPathRootShaCached c0 = all.getPathRoot(commits.get(0)).toShaCached();
+        final GitPathRootShaCached c2 = all.getPathRoot(commits.get(2)).toShaCached();
+        assertEquals(3, all.graph().nodes().size());
+        final GitPathRootShaCached firstNode = all.graph().nodes().iterator().next();
+        assertEquals(c2, firstNode);
+        assertTrue(Files.exists(c0));
+        assertTrue(Files.exists(c2));
+      }
+    }
   }
 
   @Test
-  void testPruneGraphRemoveRoot1() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root1 = mockNode("1");
-    GitPathRootShaCached diamond1 = mockNode("2");
-    GitPathRootShaCached diamond2 = mockNode("3");
-    GitPathRootShaCached diamond3 = mockNode("4");
-    
-    GitPathRootShaCached root2 = mockNode("5");
-    
-    GitPathRootShaCached root3 = mockNode("6");
-    GitPathRootShaCached line1 = mockNode("7");
-    
-    graph.putEdge(root1, diamond1);
-    graph.putEdge(root1, diamond2);
-    graph.putEdge(diamond1, diamond3);
-    graph.putEdge(diamond2, diamond3);
+  void testReadOnlyZero() throws Exception {
+    try (DfsRepository repo = new InMemoryRepository(new DfsRepositoryDescription("myrepo"))) {
+      final ImmutableList<ObjectId> commits = JGit.createRepoWithSubDir(repo);
+      assertEquals(3, commits.size());
+      try (GitDfsFileSystem fs =
+          GitFileSystemProvider.instance().newFileSystemFromDfsRepository(repo)) {
+        LOGGER.debug("Shas: " + fs.graph().nodes());
 
-    graph.addNode(root2);
-    
-    graph.putEdge(root3, line1);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of(root1.getStaticCommitId());
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    MutableGraph<GitPathRootShaCached> expected = GraphBuilder.directed().build();
-    expected.addNode(root2);
-    expected.addNode(root3);
-    expected.addNode(line1);
-    expected.putEdge(root3, line1);
-    assertEquals(expected, result);
+        final GitPruningFs first = GitPruningFs.prune(fs, c -> c.getCommit().id().equals(commits.get(1)));
+
+        final GitPathRootShaCached c0 = first.getPathRoot(commits.get(0)).toShaCached();
+        assertTrue(Files.exists(c0));
+
+        GitPathRootSha c2 = first.getPathRoot(commits.get(2));
+        assertThrows(NoSuchFileException.class, () -> c2.getFileSystem().provider().checkAccess(c2));
+        assertThrows(NoSuchFileException.class, () -> Files.readString(c2));
+        assertFalse(Files.exists(c2));
+        assertFalse(Files.exists(c2.resolve(first.getPath("ploum"))));
+        assertFalse(Files.exists(c2.resolve("")));
+        assertThrows(NoSuchFileException.class, () -> c2.toShaCached());
+        assertThrows(NoSuchFileException.class, () -> c2.getCommit());
+        assertEquals(ImmutableList.of(), c0.getParentCommits());
+    // BasicFileAttributeView v = c2.getFileSystem().provider().getFileAttributeView(c2, BasicFileAttributeView.class);
+    // assertThrows(NoSuchFileException.class, () -> v.readAttributes());
+      }
+    }
   }
 
   @Test
-  void testPruneGraphRemoveRoot2() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root1 = mockNode("1");
-    GitPathRootShaCached diamond1 = mockNode("2");
-    GitPathRootShaCached diamond2 = mockNode("3");
-    GitPathRootShaCached diamond3 = mockNode("4");
-    
-    GitPathRootShaCached root2 = mockNode("5");
-    
-    GitPathRootShaCached root3 = mockNode("6");
-    GitPathRootShaCached line1 = mockNode("7");
-    
-    graph.putEdge(root1, diamond1);
-    graph.putEdge(root1, diamond2);
-    graph.putEdge(diamond1, diamond3);
-    graph.putEdge(diamond2, diamond3);
+  void testGraph() throws Exception {
+    try (DfsRepository repo = new InMemoryRepository(new DfsRepositoryDescription("myrepo"))) {
+      final ImmutableList<ObjectId> commits = JGit.createRepoWithSubDir(repo);
+      assertEquals(3, commits.size());
+      try (GitDfsFileSystem fs =
+          GitFileSystemProvider.instance().newFileSystemFromDfsRepository(repo)) {
+        LOGGER.debug("Shas: " + fs.graph().nodes());
 
-    graph.addNode(root2);
-    
-    graph.putEdge(root3, line1);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of(root2.getStaticCommitId());
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    MutableGraph<GitPathRootShaCached> expected = GraphBuilder.directed().build();
-    expected.putEdge(root1, diamond1);
-    expected.putEdge(root1, diamond2);
-    expected.putEdge(diamond1, diamond3);
-    expected.putEdge(diamond2, diamond3);
-    expected.addNode(root3);
-    expected.addNode(line1);
-    expected.putEdge(root3, line1);
-    assertEquals(expected, result);
+        final GitPruningFs none = GitPruningFs.prune(fs, c -> !c.getCommit().id().equals(null));
+        assertEquals(0, none.graph().nodes().size());
+        final GitPruningFs first = GitPruningFs.prune(fs, c -> !c.getCommit().id().equals(commits.get(0)));
+        assertEquals(1, first.graph().nodes().size());
+        assertEquals(first.getPathRoot(commits.get(0)),
+            Iterables.getOnlyElement(first.graph().nodes()));
+
+        final GitPruningFs middle =
+            GitPruningFs.prune(fs, c -> c.getCommit().id().equals(commits.get(2)));
+        final ImmutableGraph<GitPathRootShaCached> graph = middle.graph();
+        LOGGER.debug("Middle: {}.", graph);
+        assertEquals(2, graph.nodes().size());
+        final ImmutableSet<ObjectId> middleIds = graph.nodes().stream().map(p -> p.getCommit().id())
+            .collect(ImmutableSet.toImmutableSet());
+        assertEquals(ImmutableSet.of(commits.get(0), commits.get(1)), middleIds);
+        final GitPathRootShaCached c0 = middle.getPathRoot(commits.get(0)).toShaCached();
+        final GitPathRootShaCached c1 = middle.getPathRoot(commits.get(1)).toShaCached();
+        assertThrows(NoSuchFileException.class, () -> middle.getPathRoot(commits.get(2)).toShaCached());
+        assertEquals(ImmutableSet.of(c0), graph.predecessors(c1));
+        assertEquals(ImmutableSet.of(), graph.predecessors(c0));
+      }
+    }
   }
 
   @Test
-  void testPruneGraphRemoveDiamond2() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root1 = mockNode("1");
-    GitPathRootShaCached diamond1 = mockNode("2");
-    GitPathRootShaCached diamond2 = mockNode("3");
-    GitPathRootShaCached diamond3 = mockNode("4");
-    
-    GitPathRootShaCached root2 = mockNode("5");
-    
-    GitPathRootShaCached root3 = mockNode("6");
-    GitPathRootShaCached line1 = mockNode("7");
-    
-    graph.putEdge(root1, diamond1);
-    graph.putEdge(root1, diamond2);
-    graph.putEdge(diamond1, diamond3);
-    graph.putEdge(diamond2, diamond3);
+  void testDiff() throws Exception {
+    try (DfsRepository repo = new InMemoryRepository(new DfsRepositoryDescription("myrepo"))) {
+      final ImmutableList<ObjectId> commits = JGit.createRepoWithSubDir(repo);
+      try (GitDfsFileSystem fs =
+          GitFileSystemProvider.instance().newFileSystemFromDfsRepository(repo)) {
+        final GitPruningFs gitFs = GitPruningFs.prune(fs, c -> c.getCommit().id().equals(commits.get(2)));
+        final GitPathRootSha p0 = gitFs.getPathRoot(commits.get(0));
+        final GitPathRootSha p1 = gitFs.getPathRoot(commits.get(1));
+        final GitPathRootSha p2 = gitFs.getPathRoot(commits.get(2));
 
-    graph.addNode(root2);
-    
-    graph.putEdge(root3, line1);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of(diamond2.getStaticCommitId(), root3.getStaticCommitId());
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    MutableGraph<GitPathRootShaCached> expected = GraphBuilder.directed().build();
-    expected.putEdge(root1, diamond1);
-    expected.addNode(root2);
-    assertEquals(expected, result);
-  }
-
-  @Test
-  void testPruneGraphFailsToAddEdges() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root = mockNode("1");
-    GitPathRootShaCached child1 = mockNode("2");
-    GitPathRootShaCached child2 = mockNode("3");
-    
-    graph.putEdge(root, child1);
-    graph.putEdge(child1, child2);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of();
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    assertEquals(3, result.nodes().size());
-    assertEquals(2, result.edges().size());
-    assertTrue(result.hasEdgeConnecting(root, child1));
-    assertTrue(result.hasEdgeConnecting(child1, child2));
-  }
-
-  @Test
-  void testPruneGraphWithCyclicRevisit() {
-    MutableGraph<GitPathRootShaCached> graph = GraphBuilder.directed().build();
-    
-    GitPathRootShaCached root1 = mockNode("1");
-    GitPathRootShaCached root2 = mockNode("2");
-    GitPathRootShaCached shared = mockNode("3");
-    GitPathRootShaCached child = mockNode("4");
-    
-    graph.putEdge(root1, shared);
-    graph.putEdge(root2, shared);
-    graph.putEdge(shared, child);
-    
-    ImmutableSet<ObjectId> invisibleStarts = ImmutableSet.of(root2.getStaticCommitId());
-    
-    Graph<GitPathRootShaCached> result = GitPruningFs.pruneGraph(graph, p -> invisibleStarts.contains(p.getStaticCommitId()));
-    
-    MutableGraph<GitPathRootShaCached> expected = GraphBuilder.directed().build();
-    expected.addNode(root1);
-    
-    assertEquals(expected, result);
-    assertEquals(1, result.nodes().size());
-    assertEquals(0, result.edges().size());
-    assertTrue(result.nodes().contains(root1));
+        assertEquals(ImmutableSet.of(), gitFs.diff(p0, p0));
+        {
+          assertThrows(NoSuchFileException.class, () -> gitFs.diff(p0, p2));
+        }
+        {
+          assertThrows(NoSuchFileException.class, () -> gitFs.diff(p2, p0));
+        }
+        {
+          assertThrows(NoSuchFileException.class, () -> gitFs.diff(p1, p2));
+        }
+        {
+          final ImmutableSet<DiffEntry> diffs01 = gitFs.diff(p0, p1);
+          final UnmodifiableIterator<DiffEntry> iterator = diffs01.iterator();
+          final DiffEntry diff01 = iterator.next();
+          assertFalse(iterator.hasNext());
+          assertEquals(ChangeType.ADD, diff01.getChangeType());
+          assertEquals("file2.txt", diff01.getNewPath());
+        }
+      }
+    }
   }
 }
